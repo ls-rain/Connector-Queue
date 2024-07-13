7d56544e4d93f661a98c29ac502d295614a8490d
ls-rain <swlee19990115@gmail.com>
2024-07-13 22:59:14 +0900

    Initial commit for flow

---
+++ src/test/java/com/example/webfluxflow/WebfluxflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
